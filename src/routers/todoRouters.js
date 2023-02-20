0b48d879a8ae257c821de7a918701b115d9712a9
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2023-02-20 20:17:51 +0300

    Todo Back-End Bitti

--- src/routers/todoRouters.js
+++ src/routers/todoRouters.js
@@ -4,4 +4,12 @@ const todoController = require("../controllers/todoController");
 
 router.post("/todo", todoController.todoAdd);
 
+router.get("/todo", todoController.todoGetAll);
+
+router.put("/todo/:id", todoController.todoUpdate);
+
+router.delete("/todo/:id", todoController.todoDelete);
+
+router.get("/todo/:id", todoController.todoGet);
+
 module.exports = router;
